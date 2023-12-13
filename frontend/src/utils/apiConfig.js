b7b60f0d35b6c6cab5677540b8f95bf163e000c2
Teegroon-on <teegroon@gmail.com>
2023-12-14 00:07:33 +0300

    change api url

--- frontend/src/utils/apiConfig.js
+++ frontend/src/utils/apiConfig.js
@@ -1,6 +1,6 @@
 
 export const apiConfig = {
-  baseUrl: `api.sergey.domain.mesto.nomoredomainsmonster.ru`,
+  baseUrl: `https://api.sergey.domain.mesto.nomoredomainsmonster.ru`,
   headers: {
     "Content-Type": "application/json",
   },
